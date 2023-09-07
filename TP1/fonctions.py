b23c7399d00ffa74177c8058da9a2b3933f9ed94
RravielL <julientonon22@gmail.com>
2023-09-07 16:11:42 +0200

    workflow

---
+++ TP1/fonctions.py
@@ -0,0 +1,6 @@
+def puissance(a,b):
+	if type(a) and type (b) is int :
+		return a**b
+	else :
+		raise TypeError("Only integers are allowed")
+
--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,9 @@
 print("Hello, World ! ")
+
+"""while True :
+	saisir=float(input("Saisir un nombre : "))
+	carre = saisir*saisir
+	print(carre)
+"""
+import fonctions as f
+res = f.puissance(int(input("Saisir un nombre entier : ")),int(input("Saisir un nombre entier : ")))
