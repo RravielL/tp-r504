fc565355590ba291d5cfb5cd391bfae921b8c10a
RravielL <julientonon22@gmail.com>
2023-09-27 17:02:56 +0200

    oui

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,6 +1,18 @@
 def puissance(a,b):
+	if a==0 and b<0 :
+		raise TypeError("The power of zero is undefined for a negative exponant") 
+	if b==0 :
+		return 1
 	if type(a) and type (b) is int :
-		return a**b
+		c=1
+		if b<0:
+			for i in range (b*(-1)):
+				c=c/a
+			return c	
+		if b>=0:
+			for i in range (b):
+				c=a*c
+			return c
 	else :
 		raise TypeError("Only integers are allowed")
 
--- TP1/test.py
+++ TP1/test.py
@@ -8,10 +8,11 @@ def test_2():
 	assert f.puissance(2,-2)==0.25
 	assert f.puissance(10,-4)==0.0001
 def test_3():
-	assert f.puissance(2,2) ==4
 	assert f.puissance(-2,2)==4
-	assert f.puissance(2,0)==1 #marche pas
-	assert f.puissance(-2,0)==-1
+	assert f.puissance(-2,3)==-8
+	assert f.puissance(2,0)==1
+	assert f.puissance(-2,0)==1
 	assert f.puissance(0,0)==1
 	assert f.puissance(0,3)==0
-	assert f.puissance(0,-3)==0 #marche pas
+	
+	with pytest.raises(TypeError("The power of zero is undefined for a negative exponant")): f.puissance(0,-3)
